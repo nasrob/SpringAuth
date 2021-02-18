507c110109642030dff0f7b80abe88ab7b9a4f68
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-02-18 20:00:19 +0100

    added spring security config

--- src/main/java/com/nboukehil/springldap/config/LdapDataConfig.java
+++ src/main/java/com/nboukehil/springldap/config/LdapDataConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.nboukehil.springldap.model.LdapAuthStructure;
+
 @Configuration
 @ComponentScan(basePackages = {"com.nboukehil.springldap.config"})
 public class LdapDataConfig {
@@ -21,5 +23,17 @@ public class LdapDataConfig {
 	@Value("${spring.ldap.username}")
 	private String ldapManagerUserName;
 	
-//	@Bean("ldapAuthStrecture")
+	@Bean("ldapAuthStrecture")
+	public LdapAuthStructure getLdapAuthStructure() {
+		LdapAuthStructure authStructure = new LdapAuthStructure();
+		
+		authStructure.setLdapUrl(ldapUrls);
+		authStructure.setLdapBase(ldapBase);
+		authStructure.setLdapManagerDn(ldapManagerUserName);
+		authStructure.setLdapManagerPwd(ldapManagerPwd);
+		authStructure.setUserDnPattern("uid={0},ou=users");
+		authStructure.setUserSearchBase("ou=roles");
+		
+		return authStructure;
+	}
 }
